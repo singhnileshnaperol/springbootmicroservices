fc96fc986fd1a2056741c79860e6bbae62f4d718
Nilesh Singh <singhnilesh0291@gmail.com>
2019-12-21 19:48:11 +0530

    First commit

---
+++ microservices/product-recommendation/src/main/java/com/springbootapp/productrecommendation/ProductRecommendationApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapp.productrecommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductRecommendationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductRecommendationApplication.class, args);
+	}
+
+}
---
+++ microservices/product-recommendation/src/test/java/com/springbootapp/productrecommendation/ProductRecommendationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootapp.productrecommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductRecommendationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
